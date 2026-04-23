24616e8eb5b567f4351fc915f680e9b554c99e87
CDalianis <christos_dl@hotmail.com>
2026-04-23 11:10:45 +0300

    init commit

--- src/main/java/com/example/mapstest/controller/LocationController.java
+++ src/main/java/com/example/mapstest/controller/LocationController.java
@@ -3,7 +3,7 @@ package com.example.mapstest.controller;
 import com.example.mapstest.model.Location;
 import com.example.mapstest.service.LocationService;
 import org.springframework.web.bind.annotation.*;
-
+//import lib
 import java.util.List;
 
 @RestController
