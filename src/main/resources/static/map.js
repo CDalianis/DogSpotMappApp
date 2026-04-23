a1450f8ce0e0a91a4621567ce5a0bb1cae6cc092
CDalianis <christos_dl@hotmail.com>
2026-04-23 11:06:42 +0300

    init commit

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,5 +1,6 @@
 var map = L.map('map').setView([37.9838, 23.7275], 13);
 
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
