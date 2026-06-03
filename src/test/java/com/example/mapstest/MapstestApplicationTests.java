7fe21560302f7d250e984be5914109b10ae6cd72
CDalianis <christos_dl@hotmail.com>
2026-06-03 13:40:26 +0300

    fix: map test import

---
+++ src/test/java/com/example/mapstest/MapstestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.mapstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
